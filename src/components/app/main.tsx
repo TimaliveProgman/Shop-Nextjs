778c2c735f85079b295d8d2713ecb5ecee12a821
TimaliveProgman <timur20051610@gmail.com>
2024-06-05 15:36:35 +0500

    first commit

---
+++ src/components/app/main.tsx
@@ -0,0 +1,12 @@
+import HeaderWeb from "../header-web/header";
+
+
+
+
+export default function MainComponent() {
+    return (
+        <div className="main_component">
+            <HeaderWeb></HeaderWeb>
+        </div>
+    );
+}
---
+++ src/components/header-web/header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface IHeaderWebProps {
+}
+
+export default function HeaderWeb (props: IHeaderWebProps) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
